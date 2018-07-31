623d9434bbf45676fb0d900c6f19c80bb4869289
왕대성 <Mamosoo@wangdaeseong-ui-MacBook-Pro.local>
2018-07-31 15:02:23 +0900

    init

---
+++ 조건문/6.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+</head>
+<body>
+    <h1>javascript</h1>
+    <script>
+        result = (1==1);
+        if(result){
+            document.write("참");
+        }
+        else{
+            document.write("거짓");
+        }
+        </script>
+    <h1>php</h1>
+
+    <?php
+    $result = (1==2);
+
+    if($result){
+        echo "true";
+    }
+    else{
+        echo "false";
+    }
+    ?>
+
+</body>
+</html>
