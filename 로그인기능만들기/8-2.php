3ac3c7e6a1338afec71bb76b891b3dc5aeb11544
왕대성 <Mamosoo@wangdaeseong-ui-MacBook-Pro.local>
2018-07-31 15:21:48 +0900

    init

---
+++ 로그인기능만들기/8-2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+</head>
+<body>
+    <?php
+
+       $password = $_GET["password"];
+
+       if($password == 1111)
+       {
+           echo "환영^_^";
+       }
+       else
+        echo "Bye!";
+
+    ?>
+    
+</body>
+</html>
