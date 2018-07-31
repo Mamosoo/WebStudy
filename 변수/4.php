4655f79d5338f589e14a950ee6e2cc25f653b4ca
왕대성 <Mamosoo@wangdaeseong-ui-MacBook-Pro.local>
2018-07-31 14:36:38 +0900

    init

---
+++ 변수/4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+</head>
+<body>
+    <h1>javascript</h1>
+    <script>
+        name = "wang";
+        document.write("안녕하세요"+name);
+        </script>
+<h1>php</h1>
+    <?php
+    $name = "wang";
+    echo "안녕하세요".$name;
+?>
+    
+</body>
+</html>
